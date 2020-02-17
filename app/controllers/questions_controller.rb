dc6997b02b219ac0d3ffe47ca70aeb9039e08052
Diana King <diana@daytripperstravel.com>
2020-02-17 17:45:27 -0600

    new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
-    @question = params[:question]
   end
 
   def answer
-    @question = params[:question]
-    if answer.include? '?'
-      'Silly question, get dressed and go to work!'
-    elsif answer == 'I am going to work right now!'
-      ''
+    if answer.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      'I dont care, get dressed and go to work!'
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
